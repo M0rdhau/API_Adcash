35d1d3e0162a6bcc2e1c5f1cc40fb9851a149ee1
Dachi Mshvidobadze <dachimshvidobadze@gmail.com>
2021-03-19 21:53:38 +0200

    added mongoose models. working on controllers next

---
+++ models/productModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category'
+  }
+});
+
+productSchema.plugin(uniqueValidator);
+
+productSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+const ProductModel = mongoose.model('Product', productSchema);
+
+module.exports = ProductModel;
